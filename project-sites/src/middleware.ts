7ecff38d59d33786da2ce3c93a740e91fe639688
rounakneema <neemarounak9171@gmail.com>
2026-09-25 02:29:22 +0530

    fix(middleware): exclude robots.txt and sitemap.xml from rewrites

--- project-sites/src/middleware.ts
+++ project-sites/src/middleware.ts
@@ -9,8 +9,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico, icon.png, public static assets
+     * - robots.txt, sitemap.xml
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|icon.png).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|icon.png|robots.txt|sitemap.xml).*)',
   ],
 };
 
